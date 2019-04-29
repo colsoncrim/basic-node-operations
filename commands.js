bac1655de365611370b5886c00c51eaba8e03cff
colsoncrim <colsonscrim@gmail.com>
2019-04-29 11:36:35 -0700

    Basic node operations assignment

---
+++ commands.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+
+function done(output) {
+    process.stdout.write(output);
+    process.stdout.write('\nprompt > ');
+}
+
+function evaluateCmd(userInput) {
+  const userInputArray = userInput.split(" ");
+  const command = userInputArray[0];
+
+  switch (command) {
+    case "echo":
+      commandLibrary.echo(userInputArray.slice(1).join(" "));
+      break;
+    case "cat":
+      commandLibrary.cat(userInputArray.slice(1));
+      break;
+    case "head":
+      commandLibrary.head(userInputArray.slice(1));
+      break;
+    case "tail":
+      commandLibrary.tail(userInputArray.slice(1));
+      break;
+    }
+}
+
+const commandLibrary = {
+  "echo": function(userInput) {
+    done(userInput);
+  },
+
+  "cat": function(fullPath) {
+     const fileName = fullPath[0];
+     fs.readFile(fileName, (err, data) => {
+         if (err) throw err;
+         done(data);
+     });
+
+  "head": function(fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      done(data);
+    });
+  },
+  "tail": function (fullPath) {
+    const fileName = fullPath[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      done(data);
+    })
+  }
+  default: process.stdout.write('Command entered is invalid');
+}
+
+};
+
+module.exports.commandLibrary = commandLibrary;
+module.exports.evaluateCmd = evaluateCmd;
